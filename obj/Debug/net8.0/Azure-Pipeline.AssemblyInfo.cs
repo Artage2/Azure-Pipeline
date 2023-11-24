22c6b37267aab14702ca3a13c0f7a5c4d416c61a
Arturas Gelusevicius <arturasgelusevicius@gmail.com>
2023-11-24 14:09:54 +0100

    pipe.yaml

--- obj/Debug/net8.0/Azure-Pipeline.AssemblyInfo.cs
+++ obj/Debug/net8.0/Azure-Pipeline.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Azure-Pipeline")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7e9b20547c2d004df9aba99b23e1a86f0879f680")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Azure-Pipeline")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Azure-Pipeline")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
